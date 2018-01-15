9ac29eaf53578c0786d2c3c22ba0160021929f30
Zhihao Tan <tanzhihao2015@163.com>
2018-01-15 11:08:48 +0200

    Add files via upload

---
+++ static/middle.js
@@ -0,0 +1,24 @@
+
+
+function post(url,data){
+ var request = new XMLHttpRequest();
+ request.open("POST", url, false);
+ request.setRequestHeader("Authorization", "Basic " + btoa("admin:password123"));
+ request.setRequestHeader("X-CSRFToken", readCookie("csrftoken"));
+ request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+ request.send(data); 
+ var array = JSON.parse(request.responseText);
+ console.log(array);
+}
+
+
+function readCookie(name) {
+ var nameEQ = encodeURIComponent(name) + "=";
+ var ca = document.cookie.split(';');
+ for (var i = 0; i < ca.length; i++) {
+ var c = ca[i];
+ while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+ if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+ }
+ return null;
+}
